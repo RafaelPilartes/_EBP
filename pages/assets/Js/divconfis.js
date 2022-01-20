3c7bcffef579406ae533eea5fc9d125b23889eec
RafaelPilartes <rafaelpilartes.rlps@gmail.com>
2022-01-20 20:18:43 +0100

    4º - Versão 1.0 (acerto text e transition)

--- pages/assets/Js/divconfis.js
+++ pages/assets/Js/divconfis.js
@@ -1,18 +1,10 @@
 const selected = document.querySelector(".selected");
 const optionsContainer = document.querySelector(".options-container");
-const searchBox = document.querySelector(".search-box input");
 
 const optionsList = document.querySelectorAll(".option");
 
 selected.addEventListener("click", () => {
   optionsContainer.classList.toggle("active");
-
-  searchBox.value = "";
-  filterList("");
-
-  if (optionsContainer.classList.contains("active")) {
-    searchBox.focus();
-  }
 });
 
 optionsList.forEach(o => {
@@ -21,19 +13,3 @@ optionsList.forEach(o => {
     optionsContainer.classList.remove("active");
   });
 });
-
-searchBox.addEventListener("keyup", function(e) {
-  filterList(e.target.value);
-});
-
-const filterList = searchTerm => {
-  searchTerm = searchTerm.toLowerCase();
-  optionsList.forEach(option => {
-    let label = option.firstElementChild.nextElementSibling.innerText.toLowerCase();
-    if (label.indexOf(searchTerm) != -1) {
-      option.style.display = "block";
-    } else {
-      option.style.display = "none";
-    }
-  });
-};
